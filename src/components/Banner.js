576d63a98b7b149150faed326603c2a71f5080de
Mohamed Moubarak <moubarakmohamed81@yahoo.fr>
2021-07-31 21:23:51 +0100

    structure du projet

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import logo from '../assets/logo.png'
 import '../css/Banner.css'
@@ -5,11 +6,19 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
 const Banner = () => {
-    const title = 'Electronic Shop'
-    return(
-        <div className="float-end">
-            <img src={logo} className="rounded float-end" alt="Site de composants electroniques" />
-            <h1 className="elect-title"> {title} </h1>
+    return (
+        <div >
+            <div className="banner">
+                <a href="#">
+                    <img src={logo} className="rounded float-start" alt="Site de composants electroniques" />
+                </a>
+                <h6> Contactez-nous au <br /> <span>+237 697349179 </span></h6>
+                <h5>Tous vos composants <span>A petit prix</span> </h5>
+            </div>
+            <nav className="navbar navbar-dark bg-dark">
+                
+  
+            </nav>
 
         </div>
     )
